9fd9c1826f1574644fead40ddee761ba7e185535
Sebastian Plocek <sebastian@plocek.at>
2020-01-03 20:48:26 +0100

    Use rescript config details in favor of parameters. Add colored output. Log to zabbix using zabbix-trapper. Add CTRL-C Trap. Add Backup without encryption.

--- README.md
+++ README.md
@@ -1,31 +1,39 @@
-# lvm-restic-backup
+# LVM restic & rescript backup
 
-This bash script backups logical volumes via [restic](https://restic.net/). 
+This bash script backups logical volumes via [restic](https://restic.net/). To simplify repo handling, this script takes repo details from [rescript](https://gitlab.com/sulfuror/rescript.sh) config.
 
-## How to backup
+## Requirements
+- [restic](https://restic.net/)
+- [rescript](https://gitlab.com/sulfuror/rescript.sh)
 
-### Backup one LV
+## Optional Req. for Zabbix Monitoring
+- zabbix-sender
+- pip
+- python: humanfriendly
+- Zabbix [Rescript Restic Backup Template](https://github.com/sebastian13/zabbix-templates/tree/master/rescript-restic-backup)
 
-```
-./lvm-restic-backup.sh --repo example.env --backup ubuntu-disk --block-level
-```
+## How to use
 
-### Backup multiple LVs
-
-```
-./lvm-restic-backup.sh --repo example.env --backup list.txt --block-level
-```
-
-## How to restore
-
-### Restore one LV
-
-```
-./lvm-restic-backup.sh --repo example.env --restore ubuntu-disk --vg VG0
+```bash
+lvm-rescript [repo_name] [command] [lv_name|path-to-list]
 ```
 
-### Restore multiple LVs
+### Commands
+- `block-level-backup` creates a lvm-snapshot & pipes the volume using dd to restic
+- `block-level-gz-backup` creates a lvm-snapshot & pipes the volume using dd and pigz to restic
+- `file-level-backup` creates a lvm-snapshot & creates a restic backup using the mounted snapshot
+- `restore` restores logical volume(s)
 
+## How to install
+
+```bash
+curl -o /usr/bin/lvm-rescript https://raw.githubusercontent.com/sebastian13/lvm-restic-backup/master/lvm-restic-backup.sh
+chmod +x /usr/bin/lvm-rescript
 ```
-./lvm-restic-backup.sh --repo example.env --restore list.txt --vg VG0
+
+For Zabbix LVM Discovery also download the [script](https://github.com/sebastian13/zabbix-templates/tree/master/rescript-restic-backup):
+
+```bash
+curl -o /etc/zabbix/scripts/rescript-lvm-discovery.pl https://raw.githubusercontent.com/sebastian13/zabbix-templates/master/rescript-restic-backup/scripts/rescript-lvm-discovery.pl
+chmod +x /etc/zabbix/scripts/rescript-lvm-discovery.pl
 ```
--- lvm-restic-backup.sh
+++ lvm-restic-backup.sh
@@ -4,46 +4,42 @@
 #   How To Use
 # -------------------------------------------------------
 
-function help {
-	echo
-	echo "LVM RESTIC BACKUP"
-	echo "------------------------"
-	echo
-	echo "Parameters:"
-	echo "  --init                             Initialize the Restic Repo"
-	echo "  --repo [path-to-env-file]          Repos Information"
-	echo "  --backup [path-to-list|lv-name]    Run the backup task"
-	echo "  --block-level                      Backup the LV as RAW Image"
-	echo "  --file-level                       Backup the files inside the LV"
-	echo "  --help                             Display this help message"
-	echo
-	echo "Example:"
-	echo "  ./lvm-restic-backup.sh --repo /root/env.txt --backup ubuntu-disk --block-level"
-	echo
-	echo "LVM RESTIC RESTORE"
-	echo "------------------------"
-	echo
-	echo "Parameters:"
-	echo "  --restore [path-to-list|lv-name]   Restore a logical volume"
-	echo "  --vg [vg-name]                     Define the destination volume group"
-	echo "  --help                             Display this help message"
-	echo
-	echo "Example:"
-	echo "  ./lvm-restic-backup.sh --repo example.env --restore example-lv --vg VG0"
-	echo
-}
-
 # Exit if any statement returns a non-true value
 set -e
 
+# Define various output colors
+cecho () {
+  local _color=$1; shift
+  echo -e "$(tput setaf $_color)$@$(tput sgr0)"
+}
+black=0; red=1; green=2; yellow=3; blue=4; pink=5; cyan=6; white=7;
+
+help () {
+	echo
+	cecho $blue "LVM RESCRIPT & RESTIC BACKUP"
+	cecho $blue "----------------------------"
+	cecho $blue "Author:  Sebastian Plocek"
+	cecho $blue "URL:     https://github.com/sebastian13/lvm-restic-backup"
+	echo
+	cecho $blue  "Usage:"
+	cecho $blue  "  lvm-rescript [repo_name] [command] [lv_name|path-to-list]"
+	echo
+	cecho $blue  "Commands:"
+	cecho $blue  "  block-level-backup          Creates a lvm-snapshot & pipes the volume using dd to restic"
+	cecho $blue  "  block-level-gz-backup       Creates a lvm-snapshot & pipes the volume using dd and pigz to restic"
+	cecho $blue  "  file-level-backup           Creates a lvm-snapshot & creates a restic backup using the mounted snapshot"
+	cecho $blue  "  restore                     Restores logical volume(s)"
+	echo
+	cecho $blue  "Logical Volume:"
+	cecho $blue  "  Provide the LV name without VG."
+	cecho $blue  "  Provide the path to a list of LV names. LVs listed as #comment won't be backed up."
+	echo
+}
+
 # initialise variables
 CURRDIR="$(dirname "$(readlink -f "$0")")"
-RESTIC_ENVIRONMENT="/etc/restic/repos/default.repo"
-RESTIC_INIT=false
 RESTIC_EXCLUDE="/etc/restic/exclude.txt"
 LVM_SNAPSHOT_BUFFER="10G"
-BLOCK_LEVEL_BACKUP=false
-FILE_LEVEL_BACKUP=false
 WORKDIR="/"
 
 # Change to WORKDIR
@@ -51,13 +47,16 @@ WORKDIR="/"
 cd $WORKDIR
 
 # Create Log Directory
-mkdir -p /var/log/backup
+LOGDIR="/var/log/lvm-restic"
+mkdir -p $LOGDIR
+RLOG="/tmp/lvm-restic.log"
 
 # -------------------------------------------------------
 #   Check package availability
 # -------------------------------------------------------
 
 command -v restic >/dev/null 2>&1 || { echo "[Error] Please install restic"; exit 1; }
+command -v rescript >/dev/null 2>&1 || { echo "[Error] Please install rescript"; exit 1; }
 command -v pip >/dev/null 2>&1 || { echo "[Error] Please install python-pip"; exit 1; }
 python -c 'import humanfriendly' >/dev/null 2>&1 || { echo "[Error] Please run 'pip install humanfriendly'"; exit 1; } 
 
@@ -73,52 +72,48 @@ then
 	exit
 fi
 
-OPTS=$(getopt \
-	--option '' \
-	--long init,block-level,file-level,help,repo:,buffer:,backup:,restore:,vg: \
-	-n 'parse-options' \
-	-- "$@")
+case "$1" in
+	help)
+		help
+		exit
+		;;
+esac
 
-eval set -- "$OPTS"
+# -------------------------------------------------------
+#   Load Repository Details
+# -------------------------------------------------------
+repo="$1"
+rescript_dir="$HOME/.rescript"
+config_dir="$rescript_dir/config"
+config_file="$config_dir/$repo.conf"
 
-while [[ $# -gt 0 ]]
-do
-  case "$1" in
-    --repo )         RESTIC_ENVIRONMENT="$2"; shift; shift; ;;
-	--init )         RESTIC_INIT=true; shift; ;;
-	--buffer )       shift; LVM_SNAPSHOT_BUFFER="$1"; shift; ;;
-	--backup )       shift; LVS_TO_BACKUP="$1"; shift; ;;
-	--block-level )  BLOCK_LEVEL_BACKUP=true; shift; ;;
-	--file-level )   FILE_LEVEL_BACKUP=true; shift; ;;
-	--restore )      shift; LVS_TO_RESTORE="$1"; shift; ;;
-	--vg )           shift; VG="$1"; shift; ;;
-	--help )         HELP=true; shift; ;;
-	-- )             shift; break ;;
-    * )              shift; ;;
-  esac
-done
-
-# Display help
-if [ "$HELP" = true ]
-then
-	help
+# Check if repo config exists
+if [[ ! -e "$config_dir/$1.conf" && ! -e "$config_dir/$1.conf.gpg" ]] ; then
+	echo "There is no repo or command for [$1]. Indicate a valid"
+	echo "repo name or command to proceed. Run [lvm-rescript help] for usage."
 	exit
 fi
 
-# -------------------------------------------------------
-#   Load restic environment variables
-# -------------------------------------------------------
+# Check if the repository exists
+echo "[INFO] Looking for your restic repository. Please be patient."
+rescript ${repo} snapshots > /dev/null 2>&1 || { echo "[Error]"; rescript ${repo} snapshots; exit 1; }
 
-if [ ! -f "$RESTIC_ENVIRONMENT" ]
-then
-    echo "[Error] Your Restic Repo file is missing! Create a file named default.repo"
-    echo "        in /etc/restic/repos or specify the path using the --repo option!"
-    exit 1
+source "$config_file"
+export RESTIC_REPOSITORY="$RESTIC_REPO"
+export B2_ACCOUNT_ID="$B2_ID"
+export B2_ACCOUNT_KEY="$B2_KEY"
+export AWS_ACCESS_KEY_ID="$AWS_ID"
+export AWS_SECRET_ACCESS_KEY="$AWS_KEY"
+export AZURE_ACCOUNT_NAME="$AZURE_NAME"
+export AZURE_ACCOUNT_KEY="$AZURE_KEY"
+export GOOGLE_PROJECT_ID="$GOOGLE_ID"
+export GOOGLE_APPLICATION_CREDENTIALS="$GOOGLE_CREDENTIALS"
+if [[ "$RESCRIPT_PASS" ]] ; then
+  export RESTIC_PASSWORD="$RESCRIPT_PASS"
+else
+  export RESTIC_PASSWORD="$RESTIC_PASSWORD"
 fi
 
-set -a
-source $RESTIC_ENVIRONMENT
-set +a
 
 # -------------------------------------------------------
 #   Wait for any other restic backup to finish
@@ -130,41 +125,12 @@ do
     sleep 60
 done
 
-# -------------------------------------------------------
-#   Initialize restic repository or
-#   check if the repository is already initialized
-# -------------------------------------------------------
-
-if [ "$RESTIC_INIT" = true ]
-then
-	restic init
-	exit
-else
-	echo "[INFO] Looking for your restic repository. Please be patient."
-	restic snapshots > /dev/null 2>&1 || { echo "[Error]"; restic snapshots; exit 1; }
-fi
-
-# -------------------------------------------------------
-#   Check if variables have been provided
-# -------------------------------------------------------
-
-if [ ! "$LVS_TO_BACKUP" ] && [ ! "$LVS_TO_RESTORE" ]
-then
-    echo "[Error] Either --backup or --restore must be provided!"
-    exit 1
-fi
-
-if [ "$LVS_TO_BACKUP" ] && [ "$BLOCK_LEVEL_BACKUP" = false ] && [ "$FILE_LEVEL_BACKUP" = false ]
-then
-    echo "[Error] Tell me, if the backup should be --block-level or/and --file-level!"
-    exit 1
-fi
 
 # -------------------------------------------------------
 #   The backup tasks
 # -------------------------------------------------------
 
-function failed {
+failed () {
 	echo
 	echo " ___              ___  __  "
 	echo "|__   /\  | |    |__  |  \ "
@@ -173,7 +139,7 @@ function failed {
 	exit 1
 }
 
-function all-done {
+all-done () {
 	echo "                   __   __        ___ "
 	echo " /\  |    |       |  \ /  \ |\ | |__  "
 	echo "/--\ |___ |___    |__/ \__/ | \| |___ "
@@ -181,8 +147,69 @@ function all-done {
 	exit 0                            
 }
 
+# -------------------------------------------------------
+#   Cleaning Functions
+# -------------------------------------------------------
+
+clean-snapshot () {
+	# Look for old snapshots of ${SNAPSHOT_PATH}
+	if (lvs -o lv_path --noheadings -S "lv_attr=~[^s.*]" | grep -wo "${SNAPSHOT_PATH}")
+	then
+		cecho $red "[WARNING] ${SNAPSHOT_NAME} already exists."
+		cecho $red "          I will remove it in 5 seconds!"
+		sleep 5
+		lvremove -f ${SNAPSHOT_PATH}
+	fi
+}
+
+clean-all-snapshots () {
+	# Look for old snapshots
+	ACTIVE_SNAPSHOTS=$(lvs -o lv_path --noheadings -S "lv_attr=~[^s.*]" | tr -d '  ' | grep "_snapshot$")
+	if [ ! -z "$ACTIVE_SNAPSHOTS" ]
+	then
+		cecho $red "Removing the following active snapshots:"
+		cecho $red "${ACTIVE_SNAPSHOTS}"
+		echo
+		sleep 5
+		for i in ${ACTIVE_SNAPSHOTS}
+		do
+			lvremove -f $i
+		done
+	fi
+}
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+ctrl_c () {
+		echo
+		cecho $red "======================================================="
+		cecho $red "Trapped CTRL-C"
+		cecho $red "Signal interrupt received, cleaning up"
+		echo
+        clean-all-snapshots
+        exit 130
+}
+
+# -------------------------------------------------------
+#   Backup Functions
+# -------------------------------------------------------
+
 # Block Level Backup piped to restic
-function block-level-backup {
+block-level-backup () {
+	dd if=${SNAPSHOT_PATH} bs=4M status=none | \
+		restic backup \
+		--verbose \
+		--tag LV \
+		--tag block-level-backup \
+		--tag ${BACKUP_LV_SIZE}g_size \
+		--stdin \
+		--stdin-filename ${BACKUP_LV}.img | \
+		tee -a ${LOGDIR}/lvm-restic-backup.log | \
+		tee ${RLOG}
+	echo
+}
+
+block-level-gz-backup () {
 	dd if=${SNAPSHOT_PATH} bs=4M status=none | \
 		pigz --fast --rsyncable | \
 		restic backup \
@@ -193,29 +220,13 @@ function block-level-backup {
 		--tag ${BACKUP_LV_SIZE}g_size \
 		--stdin \
 		--stdin-filename ${BACKUP_LV}.img.gz | \
-		tee -a /var/log/backup/lvm-restic-backup.log | \
-		tee /var/log/restic/latest-backup.log
-
-	# Store Bytes Added to the Repo
-	LOG_ADDED=$( cat /var/log/restic/latest-backup.log | \
-				 grep 'Added to the repo' | awk '{print $5,$6}' | \
-				 python3 -c 'import sys; import humanfriendly; print (humanfriendly.parse_size(sys.stdin.read(), binary=True))' )
-    zabbix_sender --config /etc/zabbix/zabbix_agentd.conf --key "restic.added.[${BACKUP_LV}]" --value "$LOG_ADDED"
-
-    # Store Snapshot ID
-    LOG_SNAPSHOT=$( grep 'snapshot' /var/log/restic/latest-backup.log | awk '{print $2}')
-    zabbix_sender --config /etc/zabbix/zabbix_agentd.conf --key "restic.snapshot.[${BACKUP_LV}]" --value "$LOG_SNAPSHOT"
-
-    # Store Execution Time in Seconds
-    LOG_DURATION=$( grep 'processed' /var/log/restic/latest-backup.log | \
-					awk '{print $NF}' | \
-					awk -F':' '{print (NF>2 ? $(NF-2)*3600 : 0) + (NF>1 ? $(NF-1)*60 : 0) + $(NF)}' )
-	zabbix_sender --config /etc/zabbix/zabbix_agentd.conf --key "restic.duration.[${BACKUP_LV}]" --value "$LOG_DURATION"    
-
+		tee -a ${LOGDIR}/lvm-restic-backup.log | \
+		tee ${RLOG}
+	echo
 }
 
 # File Level Backup using restic
-function file-level-backup {
+file-level-backup () {
 	SNAPSHOT_MOUNTPOINT="/mnt/${SNAPSHOT_NAME}"
 
 	# Create the snapshot mount directory
@@ -245,21 +256,11 @@ function file-level-backup {
 	rmdir ${SNAPSHOT_MOUNTPOINT}
 }
 
-function clean-snapshots {
-	echo "Looking for old snapshots of ${SNAPSHOT_PATH}"
-	# Remove snapshot called *_snapshot
-	if (lvs -o lv_path | grep -e "\s${SNAPSHOT_PATH}\s"); then
-		echo "[WARNING] ${SNAPSHOT_NAME} already exists."
-		echo "          I will remove it in 5 seconds!"
-		sleep 5
-		lvremove -f ${SNAPSHOT_PATH}
-	else
-		echo "... Good. Nothing to clean up."
-	fi
-}
-
-function backup {
-	echo "[INFO] Starting backup $BACKUP_LV. Press CTRL-C to abort."
+snap-and-back () {
+	echo
+	cecho $blue "======================================================="
+	cecho $blue "Starting backup of LV $BACKUP_LV"
+	echo
 	sleep 5
 
 	# Get the Path + Size of the LV to Backup
@@ -277,22 +278,59 @@ function backup {
 	fi
 
 	# Check for old snapshots
-	clean-snapshots
+	clean-snapshot
 
 	# Create the snapshot
 	lvcreate --quiet -L${LVM_SNAPSHOT_BUFFER} -s -n ${SNAPSHOT_NAME} ${BACKUP_LV_PATH} > /dev/null
 
-	if [ "$BLOCK_LEVEL_BACKUP" = true ]; then block-level-backup; fi
-	if [ "$FILE_LEVEL_BACKUP" = true ]; then file-level-backup; fi
+	eval $cmd
 
 	lvremove -f ${SNAPSHOT_PATH} > /dev/null
+
+	log-backup
+}
+
+backup () {
+	zabbix-requirements
+	zabbix-discovery
+
+	if [ "${LV_TO_BACKUP}" ] 
+	then
+		if [ -f "${LV_TO_BACKUP}" ] 
+		then
+			echo "[INFO] Verifying that all listed LV exist"
+			grep -v '^#' ${LV_TO_BACKUP} | while read -r line
+			do
+				lvs --noheading -o lv_path | grep -P "/$line( |$)" || missing=true
+				#if (lvs --noheading -o lv_path | grep -P "/$line( |$)")
+			done
+
+			# If any LV is missing, exit now
+			if [ "$missing" = true ]; then echo "Not all LVs listed exist." && failed; fi
+
+			# Read the file provided and backup each LV
+			grep -v '^#' ${LV_TO_BACKUP} | while read -r line
+			do
+				BACKUP_LV=$line
+				snap-and-back
+			done
+		else
+			# Backup LV provided
+			BACKUP_LV=${LV_TO_BACKUP}
+			snap-and-back
+		fi
+	else
+		echo "LV(s) to backup missing. Please specify [lv-name] or [path-to-list]."
+		echo "Run [lvm-rescript help] for usage."
+		exit 1
+	fi
 }
 
 # -------------------------------------------------------
 #   The restore task
 # -------------------------------------------------------
 
-function restore {
+restore-lv () {
 	echo
 	echo "*** RESTORE ***"
 
@@ -342,58 +380,161 @@ function restore {
 	restic dump --path /${RESTORE_LV}.img.gz latest ${RESTORE_LV}.img.gz | \
 		unpigz | pv -s ${restore_size_int} | \
 		dd of=${RESTORE_LV_PATH} bs=4M
-
-	echo "[INFO] Done."
-	all-done
 }
 
-if [ "${LVS_TO_RESTORE}" ]
-then
-	if [ -f "${LVS_TO_RESTORE}" ] 
+select-vg () {
+	echo
+	cecho $pink "Please select one of the following Volume Group(s):"
+	select vg in $(vgs --noheading -o vg_name | tr -d '  ')
+	do
+		[ $vg ] && break
+	done
+	echo
+}
+
+restore () {
+	if [ "${LV_TO_RESTORE}" ]
 	then
-		# Read the file provided and backup each LV
-		grep -v '^#' ${LVS_TO_RESTORE} | while read -r line
-		do
-			RESTORE_LV=$line
-			restore
-		done
-		all-done
+		select-vg
+		if [ -f "${LV_TO_RESTORE}" ] 
+		then
+			# Read the file provided and backup each LV
+			grep -v '^#' ${LV_TO_RESTORE} | while read -r line
+			do
+				RESTORE_LV=$line
+				restore-lv
+			done
+		else
+			# Backup LV provided
+			RESTORE_LV=${LV_TO_RESTORE}
+			restore-lv
+		fi
 	else
-		# Backup LV provided
-		RESTORE_LV=${LVS_TO_RESTORE}
-		restore
-		all-done
+		echo "LV(s) to restore missing. Please specify [lv-name] or [path-to-list]."
+		echo "Run [lvm-rescript help] for usage."
+		exit 1
 	fi
-fi
+}
 
-# -------------------------------------------------------
-#   Get Logical Volume(s) to backup and
-#	run backup task
-# -------------------------------------------------------
-
-if [ "${LVS_TO_BACKUP}" ] 
-then
-	if [ -f "${LVS_TO_BACKUP}" ] 
+zabbix-requirements () {
+	skip_zabbix=false
+	if ! `systemctl is-active --quiet zabbix-agent`
 	then
-		echo "[INFO] Verifying that all listed LV exist"
-		grep -v '^#' ${LVS_TO_BACKUP} | while read -r line
-		do
-			lvs --noheading -o lv_path | grep -P "/$line( |$)" || (echo "Cannot find LV $line" && failed)
-		done
+		echo
+		cecho $red "Zabbix-Agent is not running. Will skip zabbix logging."
+		skip_zabbix=true
+	fi
+	if ! `command -v pip >/dev/null 2>&1`
+	then
+		cecho $red "Please install python-pip."
+		skip_zabbix=true
+	fi
+	if ! `python -c 'import humanfriendly' >/dev/null 2>&1`
+	then
+		cecho $red "Could not import python humanfriendly!"
+		cecho $red "Please run 'pip install humanfriendly'."
+		skip_zabbix=true
+	fi
+	if [ ! -f "/etc/zabbix/scripts/rescript-lvm-discovery.pl" ]
+	then
+		echo
+		cecho $red "Zabbix Script rescript-lvm-discovery.pl missing. For instructions visit:"
+		cecho $red "https://github.com/sebastian13/zabbix-templates/tree/master/rescript-restic-backup"
+		skip_zabbix=true
+	fi
+	echo
+}
 
-		# Read the file provided and backup each LV
-		grep -v '^#' ${LVS_TO_BACKUP} | while read -r line
-		do
-			BACKUP_LV=$line
-			backup
-		done
-		all-done
+zabbix-discovery () {
+	if [ $skip_zabbix = false ]
+	then
+		cecho $yellow "[Running Zabbix Discovery]"
+		export REPO="$repo"
+		export LV_TO_BACKUP="$LV_TO_BACKUP"
+		LVM_DISC=$(/etc/zabbix/scripts/rescript-lvm-discovery.pl)
+		echo "$LVM_DISC" | python -m json.tool
+		echo
+		zabbix_sender --config /etc/zabbix/zabbix_agentd.conf --key "rescript.lv.discovery" --value "$LVM_DISC"
+		echo
 	else
-		# Backup LV provided
-		BACKUP_LV=${LVS_TO_BACKUP}
+		cecho $red "[Skipping Zabbix Discovery]"
+	fi
+}
+
+log-backup () {
+	if [ $skip_zabbix = false ]
+	then
+		arr=()
+		TIME=$(stat -c '%015Y' $RLOG)
+
+		# Extract Added Bytes
+		RLOG_ADDED=$(cat $RLOG | grep 'Added to the repo' | awk '{print $5,$6}' | \
+			python3 -c 'import sys; import humanfriendly; print (humanfriendly.parse_size(sys.stdin.read(), binary=True))' )
+		arr+=("- restic.backup.added.[$BACKUP_LV.$REPO] $TIME $RLOG_ADDED")
+		echo "Bytes Added:      $RLOG_ADDED"
+
+		# Exctract Snapshot ID
+		RLOG_SNAPSHOTID=$(cat $RLOG | grep '^snapshot .* saved$' | awk '{print $2}')
+		arr+=("- restic.backup.snapshotid.[$BACKUP_LV.$REPO] $TIME $RLOG_SNAPSHOTID")
+		echo "Snapshot ID:      $RLOG_SNAPSHOTID"
+
+		# Extract Processed Time
+		RLOG_PROCESSED_TIME=$(cat $RLOG | grep '^processed.*files' | \
+				    awk '{print $NF}' | \
+				    awk -F':' '{print (NF>2 ? $(NF-2)*3600 : 0) + (NF>1 ? $(NF-1)*60 : 0) + $(NF)}' )
+		arr+=("- restic.backup.processedtime.[$BACKUP_LV.$REPO] $TIME $RLOG_PROCESSED_TIME")
+		echo "Time Processed:   $RLOG_PROCESSED_TIME"
+
+		# Extract Processed Bytes
+		RLOG_PROCESSED_BYTES=$(cat $RLOG | grep '^processed.*files' | \
+				     awk '{print $4,$5}' | \
+				     python3 -c 'import sys; import humanfriendly; print (humanfriendly.parse_size(sys.stdin.read(), binary=True))'  )
+		arr+=("- restic.backup.processedbytes.[$BACKUP_LV.$REPO] $TIME $RLOG_PROCESSED_BYTES")
+		echo "Bytes Processed:  $RLOG_PROCESSED_TIME"  
+
+		cecho $yellow "[Sending everything to Zabbix]"
+		# for ix in ${!arr[*]}; do printf "%s\n" "${arr[$ix]}"; done
+		# echo
+		send-to-zabbix () {
+			for ix in ${!arr[*]}; do printf "%s\n" "${arr[$ix]}"; done | zabbix_sender --config /etc/zabbix/zabbix_agentd.conf --with-timestamps --input-file -
+		}
+
+		# Send Data
+		# It might be the case that the Zabbix Server has not fully processed the discovery of new items yet.
+		# If sending raises an error, the script starts a second try after one minute.
+		send-to-zabbix || { cecho $red "[ERROR] Sending or processing of some items failed. Will wait one minute before trying again..."; sleep 60; send-to-zabbix; }
+		echo
+	else
+		cecho $red "[Skipping Sending Data to Zabbix]"
+	fi
+}
+
+# -------------------------------------------------------
+#   Run Selected Commands
+# -------------------------------------------------------
+cmd="$2"
+case "$cmd" in
+	"")
+		echo "Please specify a command."
+		echo "Run [lvm-rescript help] for usage."
+		exit 1
+		;;
+	block-level-backup|block-level-gz-backup|file-level-backup)
+		LV_TO_BACKUP=$3
 		backup
-		all-done
-	fi
-fi
+		;;
+	restore)
+		echo "restore selected"
+		LV_TO_RESTORE=$3
+		restore
+		;;
+	*)
+		echo "Unknown Command: ${cmd}."
+		echo "Run [lvm-rescript help] for usage."
+		exit 1
+		;;
+esac
 
-
+# Remove all remaining snapshots
+clean-all-snapshots
+all-done
